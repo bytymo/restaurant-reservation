cf579578d1a488c0c787aef2ac202a42ff019a0a
byronmosley <mosleyb91@outlook.com>
2021-04-14 12:46:58 -0700

    added filter for reservation status. completed user story 6. completed backend tests

--- back-end/src/seat/seat.service.js
+++ back-end/src/seat/seat.service.js
@@ -2,20 +2,32 @@ const knex = require('../db/connection')
 
 const read = (reservationId) =>
   knex('reservations')
-    .select('people')
+    .select('*')
     .where({ reservation_id: reservationId })
     .first()
 
-const update = (tableId, updatedTable) =>
-  knex('tables')
-    .select('*')
-    .where({ table_id: tableId })
-    .update(updatedTable, '*')
+const update = async (tableId, reservationId, updatedTable) => {
+  return knex.transaction(async function (trx) {
+    await trx('tables')
+      .select('*')
+      .where({ table_id: tableId })
+      .update(updatedTable, '*')
+    return await trx('reservations')
+      .select('*')
+      .where({ reservation_id: reservationId })
+      .update({ status: 'seated' })
+  })
+}
 
-const finish = (reservationId) => {
-  knex('reservations')
-    .where({ reservation_id: reservationId })
-    .update(status, 'finished')
+const finish = async (reservationId) => {
+  return knex.transaction(async function (trx) {
+    await trx('reservations')
+      .where({ reservation_id: reservationId })
+      .update({ status: 'finished' })
+    return await trx('tables')
+      .where({ reservation_id: reservationId })
+      .update({ reservation_id: null })
+  })
 }
 
 module.exports = {
